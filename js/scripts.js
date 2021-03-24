68f70036a4ccc1461e17652535d02c4f14b56fa7
Tori FavWest <practiceandthenpractice@gmail.com>
2021-03-24 09:46:59 -0700

    create form letter page
    Co-authored-by: Thomas Friedrichs <friedrichs.tommy@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function(){
+  $("#name-form").submit(function(event){
+    const name= $("input#name").val();
+    $("#dear-name").text(name);
+    $("#form-letter").show();
+    event.preventDefault();
+  });
+});
